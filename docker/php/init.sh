0161248a8540960db3017d85a925d292a32bf27b
David Courtey <david_courtey@yahoo.com>
2025-12-03 15:33:00 +0100

    chore: integrate Laravel Octane and update Docker for FrankenPHP compatibility

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -4,5 +4,8 @@ set -e
 if [ "$APP_ENV" = "production" ]; then
     php artisan optimize
     php artisan migrate --force
+    php artisan octane:frankenphp --host=0.0.0.0 --port=8000
+else
+    php artisan migrate --force
+    docker-php-entrypoint --config /etc/frankenphp/Caddyfile --adapter caddyfile
 fi
-docker-php-entrypoint --config /etc/frankenphp/Caddyfile --adapter caddyfile
